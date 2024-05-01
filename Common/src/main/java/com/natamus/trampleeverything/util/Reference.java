a13f7a3393edf6eaed93fa7b0336247b379487c8
Rick South <rick@natamus.com>
2024-05-01 16:18:43 +0200

    Trample Everything source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/trampleeverything/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.trampleeverything.util;
+
+public class Reference {
+	public static final String MOD_ID = "trampleeverything";
+	public static final String NAME = "Trample Everything";
+	public static final String VERSION = "3.4";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
