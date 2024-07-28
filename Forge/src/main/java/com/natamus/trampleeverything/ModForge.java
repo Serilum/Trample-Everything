212bab43f9d8e9f33b6c557a164c17a9ff6a9b30
Rick South <rick@natamus.com>
2024-07-28 07:11:53 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/trampleeverything/ModForge.java
+++ Forge/src/main/java/com/natamus/trampleeverything/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.trampleeverything;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.trampleeverything.forge.config.IntegrateForgeConfig;
 import com.natamus.trampleeverything.forge.events.ForgeTrampleEvent;
 import com.natamus.trampleeverything.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
