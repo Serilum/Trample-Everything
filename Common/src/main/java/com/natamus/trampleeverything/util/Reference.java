aa9c3a2571a8f7c3cfbfe2bd34f1a2c5d4d14ad4
Rick South <rick@natamus.com>
2026-06-18 21:16:07 +0200

    Updated Trample Everything to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/trampleeverything/util/Reference.java
+++ Common/src/main/java/com/natamus/trampleeverything/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "trampleeverything";
 	public static final String NAME = "Trample Everything";
 	public static final String VERSION = "3.7";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
